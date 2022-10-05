3e9087015add0309804e33cc66af21efdc157848
Suguru Tokuda <suguru.tokuda@gmail.com>
2022-10-05 13:32:25 -0500

    Initial Commit

---
+++ SwiftfulThinkingAdvancedLearning/SwiftfulThinkingAdvancedLearningApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftfulThinkingAdvancedLearningApp.swift
+//  SwiftfulThinkingAdvancedLearning
+//
+//  Created by Suguru on 10/5/22.
+//
+
+import SwiftUI
+
+@main
+struct SwiftfulThinkingAdvancedLearningApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
